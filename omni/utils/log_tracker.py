0997d26685672ae05451a1213e9e762b1bf23a31
vineet <vineetbhandari5110@gmail.com>
2026-06-22 01:45:51 +0530

    Update: Setup for uv and pre-commit Changes to be committed: 	new file:   .github/workflows/pr-checks.yml 	modified:   .gitignore 	new file:   .pre-commit-config.yaml 	new file:   .python-version 	modified:   logs/conversion_log.jsonl 	modified:   omni/cli.py 	modified:   omni/converters/video.py 	modified:   omni/core/dispatcher.py 	modified:   omni/utils/format_size.py 	modified:   omni/utils/format_time.py 	modified:   omni/utils/log_tracker.py 	new file:   pyproject.toml 	new file:   requirements-dev.txt 	modified:   requirements.txt 	new file:   uv.lock

--- omni/utils/log_tracker.py
+++ omni/utils/log_tracker.py
@@ -6,8 +6,15 @@ import os
 os.makedirs("logs", exist_ok=True)
 
 
-def _track_log(input_file: str,output_file : str,file_size_bytes: int, 
-               resolution: str, bitrate: float, duration: int, options: dict = None):
+def _track_log(
+    input_file: str,
+    output_file: str,
+    file_size_bytes: int,
+    resolution: str,
+    bitrate: float,
+    duration: int,
+    options: dict = None,
+):
     log_entry = {
         "timestamp": datetime.now().isoformat(),
         "input": input_file,
@@ -16,7 +23,7 @@ def _track_log(input_file: str,output_file : str,file_size_bytes: int,
         "resolution": resolution,
         "bitrate": bitrate,
         "time_taken_sec": duration,
-        "options": options
+        "options": options,
     }
 
     with open("logs/conversion_log.jsonl", "a") as f:
